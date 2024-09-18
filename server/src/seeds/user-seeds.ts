8f0efc7cbc9277046e43ede94637d645ab5c95eb
bmw-dev0p <bmw.dev0p@gmail.com>
2024-09-17 22:10:15 -0400

    completed all features and todos. coming back later for styles/addons

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -5,5 +5,8 @@ export const seedUsers = async () => {
     { username: 'JollyGuru', password: 'password' },
     { username: 'SunnyScribe', password: 'password' },
     { username: 'RadiantComet', password: 'password' },
+    { username: 'bmdubz', password: 'password' },
+    { username: 'SantaClaus', password: 'password' },
+    { username: 'JoeShmoe', password: 'password' },
   ], { individualHooks: true });
 };
